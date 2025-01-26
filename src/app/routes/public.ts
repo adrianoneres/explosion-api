2c228d74373394bee98d7bfb4c749cf3289f2b65
Adriano Souza <ah.driano@gmail.com>
2025-01-25 20:25:17 -0400

    fix: url replace

--- src/app/routes/public.ts
+++ src/app/routes/public.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const publicRoutes = Router();
 
 publicRoutes.get('/*', async (req: Request, res: Response) => {
-  const url = req.url;
+  const url = req.url.replace('/explosion-api/', '/');
   const authorization = req.headers.authorization;
 
   const response = await fetch(`https://api.brawlstars.com/v1${url}`, {
